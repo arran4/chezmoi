2518e2e6d9fd8b1bd1eeb6d306a86edcc4f3962d
Arran Ubels <arran4@gmail.com>
2023-08-03 23:29:19 +1000

    Powershell instead that's installed right?

---
+++ internal/chezmoi/lookpathin_unix_test.go
@@ -0,0 +1,42 @@
+//go:build !windows
+
+package chezmoi
+
+import "testing"
+
+func TestLookPathIn(t *testing.T) {
+	tests := []struct {
+		name    string
+		file    string
+		paths   string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Finds first",
+			file:    "sh",
+			paths:   "/usr/bin:/bin",
+			want:    "/usr/bin/sh",
+			wantErr: false,
+		},
+		{
+			name:    "Finds first 2",
+			file:    "sh",
+			paths:   "/bin:/usr/bin",
+			want:    "/bin/sh",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := LookPathIn(tt.args.file, tt.args.paths)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LookPathIn() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("LookPathIn() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
